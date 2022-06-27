fe129e1baddac8df668a637c1ea6b3869c5f3002
Ellie-exe <ryiscool247@gmail.com>
2022-06-27 04:30:46 -0400

    Simplified code

--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,21 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
     let img = image::open(&args[1]).unwrap();
-
-    let width = img.width() as usize;
-    let height = img.height() as usize;
-
     let charset = String::from(" .:-=+*#%@");
-    let mut chars = vec![vec![String::new(); width]; height];
 
-    for (column, row, pixel) in img.pixels() {
-        let value = pixel.to_luma().0[0] as f32;
-        let len = (charset.len() - 1) as f32;
+    for pixel in img.pixels() {
+        if pixel.1 % 2 != 0 { continue; }
+        if pixel.0 == 0 { println!(); }
 
-        let start = ((value / 255.0) * len).round() as usize;
-        let end = start + 1;
+        let luma = pixel.2.to_luma().0[0] as f32;
+        let charset_max = (charset.len() - 1) as f32;
+        let char = ((luma / 255.0) * charset_max).round() as usize;
 
-        let ansi = format!("\x1b[1;38;2;{};{};{}m", pixel.0[0], pixel.0[1], pixel.0[2]);
-        chars[row as usize][column as usize] = format!("{}{}", ansi, &charset[start..end]);
+        let rgb_str = format!("{};{};{}", pixel.2[0], pixel.2[1], pixel.2[2]);
+        print!("\x1b[1;38;2;{}m{}", rgb_str, &charset[char..char + 1]);
     }
 
-    for row in 0..height {
-        if row % 2 != 0 { continue; }
-
-        println!();
-
-        for column in 0..width {
-            print!("{}", chars[row][column]);
-        }
-    }
-
-    println!("\n\x1b[0m");
+    println!("\x1b[0m\n");
 }
